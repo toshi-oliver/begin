31d80947ed3318d62856b86f49919618aef03b5c
toshi-oliver <igpoid@gmail.com>
2018-12-23 11:34:22 +0900

    use the heardocument

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,25 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+$html = <<<E0F
+<html>
+<head>
+<title>Hello</title>
+<style>
+body {font-size:16pt; color:#999; }
+h1 { font-size:100pt; text-align:right; color:#eee;
+    margin:-40px 0px -50px 0px; }
+</style>
+</head>
+<body>
+    <h1>Hello</h1>
+    <p>This is sample page.</p>
+    <p>これは、サンプルで作ったページです。</p>
+</body>
+</html>
+
+E0F;
+Route::get('hello',function () use ($html) {
+    return $html;
+});
