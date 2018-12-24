94cbe6241343031611da2c314c2a5c95f297f877
toshi-oliver <igpoid@gmail.com>
2018-12-24 17:13:03 +0900

    use the Rquest&ResponseObject

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,27 +4,33 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Http\Response;
+
 class HelloController extends Controller
 {
 
-    public function __invoke() {
+    public function index(Request $request, Response $response) {
 
-        return <<<E0F
+$html = <<<E0F
 <html>
 <head>
-<title>Hello</title>
+<title>Hello/Index</title>
 <style>
-body {font-size:16pot; color:#999; }
-h1 { font-size:30pt; text-align:right; color:#eee;
-    margin:-15px 0px 0px 0px; }
+body {font-size:16pt; color:#999; }
+h1 { font-size:120pt; text-align:right; color:#fafafa;
+    margin:-50px 0px -120px 0px; }
 </style>
 </head>
 <body>
-    <h1>Single Action<h1>
-    <p>これは、シングルアクションコントローラのアクションです。</p>
+    <h1>Hello<h1>
+    <h3>Request</h3>
+    <pre>{$request}</pre>
+    <h3>Response</h3>
+    <pre>{$response}</pre>
 </body>
 </html>
 E0F;
-
+        $response->setContent($html);
+        return $response;
     }
 }
