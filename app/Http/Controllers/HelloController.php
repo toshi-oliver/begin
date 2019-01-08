2f6dff8b37d27052a39f28ab5bc88dadddb0b84f
toshi-oliver <igpoid@gmail.com>
2019-01-08 23:32:57 +0900

    use the query_string

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -9,9 +9,12 @@ use Illuminate\Http\Response;
 class HelloController extends Controller
 {
 
-    public function index()
+    public function index(Request $request)
     {
-        $data = ['msg'=>'これはコントローラから渡されたメッセージです。'];
+        $data = [
+            'msg'=>'これはコントローラから渡されたメッセージです。',
+            'id'=>$request->id
+        ];
         return view('hello.index', $data);
     }
 }
